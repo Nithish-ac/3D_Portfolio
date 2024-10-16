ccd23191463408cdfc8322ba4b1ded2e45660748
Nithish Kumar <nithishkumarstar10@gmail.com>
2024-10-16 23:52:59 +0530

    upto balls Completed

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,30 +1,25 @@
-import logo from "./logo.png";
-import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
+import logo from "./logo.svg";
+import design from "./design.png";
+import ai from "./ai.png";
+import unreal from "./unreal.png";
+import unity from "./unity.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
+import csharp from "./tech/c-sharp.svg";
+import photoshop from "./tech/photoshop.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+import visualstudio from "./tech/visual-studio.png";
+import blender from "./tech/blender.png";
+import firebase from "./tech/firebase.png";
+import unityc from "./tech/unityc.png";
+import unrealc from "./tech/unrealc.png";
+import playconsole from "./tech/playconsole.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import digamend from "./company/digamend.jpeg";
+import rpgames from "./company/rpgames.jpeg";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -32,30 +27,25 @@ import tripguide from "./tripguide.png";
 
 export {
   logo,
-  backend,
-  creator,
-  mobile,
-  web,
+  design,
+  ai,
+  unreal,
+  unity,
   github,
   menu,
   close,
-  css,
-  docker,
   figma,
   git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  unityc,
+  unrealc,
+  csharp,
+  photoshop,
+  visualstudio,
+  blender,
+  firebase,
+  playconsole,
+  digamend,
+  rpgames,
   carrent,
   jobit,
   tripguide,
